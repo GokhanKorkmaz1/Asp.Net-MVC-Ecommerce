5e9c4ba85978f6e484c97ee61e765277683529e3
GokhanKorkmaz1 <107408943+GokhanKorkmaz1@users.noreply.github.com>
2022-06-13 15:32:55 +0300

    Add files via upload

---
+++ WebAppUI/ValidationRules/FluentValidation/CustomerAddValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppUI.ValidationRules.FluentValidation
+{
+    public class CustomerAddValidator:AbstractValidator<Customer>
+    {
+        public CustomerAddValidator()
+        {
+            RuleFor(s => s.Name).NotEmpty().WithMessage("Name required");
+            RuleFor(s => s.Name).MinimumLength(2);
+            RuleFor(s => s.Surname).NotEmpty().WithMessage("Surname required");
+            RuleFor(s => s.Surname).MinimumLength(2);
+            RuleFor(s => s.PhoneNumber).NotEmpty().WithMessage("Phone number required");
+            RuleFor(s => s.PhoneNumber).Length(10);
+            RuleFor(s => s.Password).NotEmpty().WithMessage("Password required");
+        }
+    }
+}
---
+++ WebAppUI/ValidationRules/FluentValidation/ProductAddValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppUI.ValidationRules.FluentValidation
+{
+    public class ProductAddValidator:AbstractValidator<Product>
+    {
+        public ProductAddValidator()
+        {
+            RuleFor(p => p.CategoryId).InclusiveBetween(1, 8);
+            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Category Id required");
+            RuleFor(p => p.ImagePath).NotEmpty().WithMessage("Image path required");
+            RuleFor(p => p.Producer).NotEmpty().WithMessage("Producer name required");
+            RuleFor(p => p.Model).NotEmpty().WithMessage("Model name required");
+            RuleFor(p => p.Price).NotEmpty().WithMessage("Price required");
+            RuleFor(p => p.UnitinStock).NotEmpty().WithMessage("Stock value required");
+        }
+    }
+}
