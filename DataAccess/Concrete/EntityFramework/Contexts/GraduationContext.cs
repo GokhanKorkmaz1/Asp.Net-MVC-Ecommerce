aee6c369e61231152c1d551cd2924e1e0881c1e7
GokhanKorkmaz1 <107408943+GokhanKorkmaz1@users.noreply.github.com>
2022-06-13 15:24:38 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/Contexts/GraduationContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class GraduationContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLOCALDB;Database=WebDatabase;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AdminTable> AdminTables { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
